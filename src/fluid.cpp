3cef3f77ae7dd8484ff2504f64d67e02d19c0abc
John <japarker@uchicago.edu>
2017-05-23 18:37:35 -0500

    added box object

--- src/fluid.cpp
+++ src/fluid.cpp
@@ -40,7 +40,7 @@ void fluid::collide_and_stream() {
     static double omega_p = 1 - omega;
 
     for (int i = 0; i < Nx; i++) {
-        for (int j = 0; j < Ny; j++) {
+        for (int j = 1; j < Ny-1; j++) {
             for (int k = 0; k < Nv; k++) {
                 // equilibrium
                 double fi_eq = wi[k]*rho(i,j) * (1 + cs2*(Ux(i,j)*cix[k] + Uy(i,j)*ciy[k])
@@ -53,7 +53,7 @@ void fluid::collide_and_stream() {
                 double fi_star = omega_p * fi(i,j,k) + omega * fi_eq;
 
                 unsigned int xi = (Nx + i + cix[k]) % Nx;
-                unsigned int yi = (Ny + j + ciy[k]) % Ny;
+                unsigned int yi = j + ciy[k];
                 // streaming
                 //fi_new(i+cix[k], j+ciy[k], k) = fi_star;
                 fi_new(xi,yi, k) = fi_star;
@@ -93,24 +93,26 @@ void fluid::update() {
     collide_and_stream();
 
     // apply boundary conditions here
-    double u_in = 0.01*3;
+    double u_in = 0.003*3;
     double rho_in = 1.01;
-    double rho_out = 0.99;
-    for (int j = 0; j < Ny; j++) {
+    double rho_out = 1.0;
+    for (int j = 1; j < Ny-1; j++) {
         // velocity BC
-        //double rho_wall = 1/(1-u_in) * (fi_new(0,j,0) + fi_new(0,j,2) + fi_new(0,j,4)
-                 //+ 2*(fi_new(0,j,3) + fi_new(0,j,6) + fi_new(0,j,7)) );
-        //fi_new(0,j,1) = fi_new(0,j,3);
-        //fi_new(0,j,5) = fi_new(0,j,7) - 0.5*(fi_new(0,j,2) - fi_new(0,j,4)) + 1.0/6*rho_wall*u_in;
-        //fi_new(0,j,8) = fi_new(0,j,6) + 0.5*(fi_new(0,j,2) - fi_new(0,j,4)) + 1.0/6*rho_wall*u_in;
+        double rho_wall = 1/(1-u_in) * (fi_new(0,j,0) + fi_new(0,j,2) + fi_new(0,j,4)
+                 + 2*(fi_new(0,j,3) + fi_new(0,j,6) + fi_new(0,j,7)) );
+        fi_new(0,j,1) = fi_new(0,j,3);
+        fi_new(0,j,5) = fi_new(0,j,7) - 0.5*(fi_new(0,j,2) - fi_new(0,j,4)) + 1.0/6*rho_wall*u_in;
+        fi_new(0,j,8) = fi_new(0,j,6) + 0.5*(fi_new(0,j,2) - fi_new(0,j,4)) + 1.0/6*rho_wall*u_in;
 
         // pressure BC
+        // left
         double ux = 1 - 1/(rho_in) * (fi_new(0,j,0) + fi_new(0,j,2) + fi_new(0,j,4)
                                  + 2*(fi_new(0,j,3) + fi_new(0,j,6) + fi_new(0,j,7)) );
-        fi_new(0,j,1) = fi_new(0,j,3) + 2/3*rho_in*ux;
-        fi_new(0,j,5) = fi_new(0,j,7) - 0.5*(fi_new(0,j,2) - fi_new(0,j,4)) + 1.0/6*rho_in*ux;
-        fi_new(0,j,8) = fi_new(0,j,6) + 0.5*(fi_new(0,j,2) - fi_new(0,j,4)) + 1.0/6*rho_in*ux;
+        //fi_new(0,j,1) = fi_new(0,j,3) + 2/3*rho_in*ux;
+        //fi_new(0,j,5) = fi_new(0,j,7) - 0.5*(fi_new(0,j,2) - fi_new(0,j,4)) + 1.0/6*rho_in*ux;
+        //fi_new(0,j,8) = fi_new(0,j,6) + 0.5*(fi_new(0,j,2) - fi_new(0,j,4)) + 1.0/6*rho_in*ux;
 
+        // right
         ux = 1 - 1/(rho_out) * (fi_new(Nx-1,j,0) + fi_new(Nx-1,j,2) + fi_new(Nx-1,j,4)
                                  + 2*(fi_new(Nx-1,j,1) + fi_new(Nx-1,j,5) + fi_new(Nx-1,j,8)) );
         fi_new(Nx-1,j,3) = fi_new(Nx-1,j,1) + 2/3*rho_out*ux;
@@ -119,22 +121,93 @@ void fluid::update() {
 
         //rho(0,j) = rho_wall;
     }
+    //fi = fi_new;
+    //t += dt;
+    //return;
     
+    // top and bottom walls
     for (int i = 1; i < Nx-1; i++) {
         fi_new(i,1,2) = fi_new(i,0,4);
         fi_new(i,1,5) = fi_new(i-1,0,7);
         fi_new(i,1,6) = fi_new(i+1,0,8);
         fi_new(i,0,4) = wi[4]; 
-        fi_new(i,0,7) = wi[7]; 
-        fi_new(i,0,8) = wi[8]; 
+        fi_new(i-1,0,7) = wi[7]; 
+        fi_new(i+1,0,8) = wi[8]; 
 
         fi_new(i,Ny-2,4) = fi_new(i,Ny-1,2);
         fi_new(i,Ny-2,7) = fi_new(i+1,Ny-1,5);
         fi_new(i,Ny-2,8) = fi_new(i-1,Ny-1,6);
         fi_new(i,Ny-1,2) = wi[2]; 
-        fi_new(i,Ny-1,5) = wi[5]; 
-        fi_new(i,Ny-1,6) = wi[6]; 
+        fi_new(i+1,Ny-1,5) = wi[5]; 
+        fi_new(i-1,Ny-1,6) = wi[6]; 
     }
+
+    // bottom left corner
+    fi_new(0,1,2) = fi_new(0,0,4);
+    fi_new(0,1,6) = fi_new(1,0,8);
+    fi_new(0,0,4) = wi[4];
+    fi_new(1,0,8) = wi[8];
+
+    // bottom right corner
+    fi_new(Nx-1,1,2) = fi_new(Nx-1,0,4);
+    fi_new(Nx-1,1,5) = fi_new(Nx-2,0,7);
+    fi_new(Nx-1,0,4) = wi[4];
+    fi_new(Nx-2,0,7) = wi[7];
+
+    // top left corner
+    fi_new(0,Ny-2,4) = fi_new(0,Ny-1,2);
+    fi_new(0,Ny-2,7) = fi_new(1,Ny-1,5);
+    fi_new(0,Ny-1,2) = wi[2];
+    fi_new(1,Ny-1,5) = wi[5];
+
+    // top right corner
+    fi_new(Nx-1,Ny-2,4) = fi_new(Nx-1,Ny-1,2);
+    fi_new(Nx-1,Ny-2,8) = fi_new(Nx-2,Ny-1,6);
+    fi_new(Nx-1,Ny-1,2) = wi[2];
+    fi_new(Nx-2,Ny-1,6) = wi[6];
+
+
+    // box
+    // left and right
+    int ia = 40, ib = 60;
+    int ja = 65, jb = 85;
+    for (int j = ja; j < jb; j++) {
+        fi_new(ia,j,3) = fi_new(ia+1,j,1);
+        fi_new(ia,j,6) = fi_new(ia+1,j-1,8);
+        fi_new(ia,j,7) = fi_new(ia+1,j+1,5);
+
+        fi_new(ia+1,j,1) = wi[1]; 
+        fi_new(ia+1,j-1,8) = wi[8]; 
+        fi_new(ia+1,j+1,5) = wi[5]; 
+
+        fi_new(ib,j,1) = fi_new(ib-1,j,3);
+        fi_new(ib,j,8) = fi_new(ib-1,j+1,6);
+        fi_new(ib,j,5) = fi_new(ib-1,j-1,7);
+
+        fi_new(ib-1,j,3) = wi[3]; 
+        fi_new(ib-1,j+1,6) = wi[6]; 
+        fi_new(ib-1,j-1,7) = wi[7]; 
+    }
+    // top and bottom
+    for (int i = ia; i < ib; i++) {
+        fi_new(i,ja,4) = fi_new(i,ja+1,2);
+        fi_new(i,ja,7) = fi_new(i+1,ja+1,5);
+        fi_new(i,ja,8) = fi_new(i-1,ja+1,6);
+
+        fi_new(i,ja+1,2) = wi[2]; 
+        fi_new(i+1,ja+1,5) = wi[5]; 
+        fi_new(i-1,ja+1,6) = wi[6]; 
+
+        fi_new(i,jb,2) = fi_new(i,jb-1,4);
+        fi_new(i,jb,6) = fi_new(i+1,jb-1,8);
+        fi_new(i,jb,5) = fi_new(i-1,jb-1,7);
+
+        fi_new(i,jb-1,4) = wi[4]; 
+        fi_new(i+1,jb-1,8) = wi[8]; 
+        fi_new(i-1,jb-1,7) = wi[7]; 
+    }
+
+
     // copy xmax to xmin, xmin to xmax
     
     fi = fi_new;
--- tests/main.cpp
+++ tests/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 using namespace latbolt;
 
 int main() {
-    fluid f("test.h5", 100, 50, 1.56);
+    fluid f("test.h5", 300, 150, 1.0);
 
     for (int i = 0; i < 1000; i++) {
         f.write_U();
